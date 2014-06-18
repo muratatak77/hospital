44c6f6acc7435e60df0b58c703b6d3412d00ae63
Soner CAN <soner@gliza.com>
2014-06-18 17:52:08 +0300

    phone added to housekeepers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140618144816) do
+ActiveRecord::Schema.define(version: 20140618145132) do
 
   create_table "doctors", force: true do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20140618144816) do
     t.string   "surname"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "phone"
   end
 
   create_table "patients", force: true do |t|
