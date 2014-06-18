b031c3a613d95d04521b01889551ef1a4d26f788
Soner CAN <soner@gliza.com>
2014-06-18 17:11:02 +0300

    Efef

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20140618140222) do
 
   create_table "doctors", force: true do |t|
@@ -29,7 +28,6 @@ ActiveRecord::Schema.define(version: 20140618140222) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
 
   create_table "housekeepers", force: true do |t|
     t.string   "name"
