ba6b9ac41689f8d66571156494b7895e44713d60
Murat Atak <murat@gliza.com>
2014-06-21 18:24:24 +0300

    djlkjs

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,15 +1,28 @@
 class Patient < ActiveRecord::Base
 
-def regards
-	"#{full_name} gecmis olsun.. nish medical"
-end
+  def regards(arg = 3)
+    "#{full_name} #{check(arg)} nish medical"
+  end
 
-private
-	
-	def full_name
-		"#{name} #{surname}"
-	end
 
-	
+
+  private
+  def check(arg)
+
+    case arg # a_variable is the variable we want to compare
+    when 1    #compare to 1
+      "Gunaydin hasta"
+    when 2    #compare to 2
+      "Gecmis olsun. Turp gibisin"
+    else
+      "Sizi kaybettik. gule gule"
+    end
+  end
+
+  def full_name
+    "#{name} #{surname}"
+  end
+
+
 
 end
