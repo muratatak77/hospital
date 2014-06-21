188061c56825a1c3833b4c940c6f39ccb6a4188e
Murat Atak <murat@gliza.com>
2014-06-21 17:14:44 +0300

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140618145132) do
+ActiveRecord::Schema.define(version: 20140618145137) do
 
   create_table "doctors", force: true do |t|
     t.string   "name"
